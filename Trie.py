25fa6a2f6edd5537dbcd7ce1991e28c007593919
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2014-03-09 19:08:58 +0100

    Update Trie.py with error handling

--- Trie.py
+++ Trie.py
@@ -5,6 +5,7 @@ from ctypes import *
 
 libtrie = cdll.LoadLibrary("./libtrie.so")
 libtrie.trie_lookup.restype = c_char_p
+libtrie.trie_get_last_error.restype = c_char_p
 
 class TrieException(Exception):
     pass
@@ -13,7 +14,8 @@ class Trie(object):
     def __init__(self, filename):
         self.ptr = libtrie.trie_load(filename)
         if self.ptr == 0:
-            raise TrieException("Failed to load library")
+            err = libtrie.trie_get_last_error()
+            raise TrieException(str(err))
 
     def lookup(self, key):
         res = libtrie.trie_lookup(self.ptr, c_char_p(key))
@@ -22,7 +24,8 @@ class Trie(object):
         else:
             return []
 
-t = Trie('prijmeni.trie')
+
+t = Trie('prijmeni2.trie')
 for s in t.lookup('Sedlář'):
     print s
 print '%s' % t.lookup('blah')
