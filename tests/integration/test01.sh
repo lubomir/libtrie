67e22bfbb501e2d5bd7466d4ac39812844f58e84
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2014-04-02 23:30:38 +0200

    Add test coverage support
    Use make coverage to compute test coverage. This will re-run the tests
    to get better results. The tests do not use valgrind. Do not install if
    coverage is enabled.

--- tests/integration/test01.sh
+++ tests/integration/test01.sh
@@ -3,6 +3,12 @@
 set -e
 set -o pipefail
 
+if [ "x${USE_VALGRIND:-yes}" = "xno" ]; then
+    VALGRIND=""
+else
+    VALGRIND="valgrind --error-exitcode=1 -q"
+fi
+
 TRIE=$(mktemp)
 COMPILE_INPUT=$(mktemp)
 COMPILE_OUTPUT=$(mktemp)
@@ -35,10 +41,10 @@ EOF
 
 make
 
-libtool --mode=execute valgrind --error-exitcode=1 -q ./list-compile "$COMPILE_INPUT" "$TRIE" \
+libtool --mode=execute $VALGRIND ./list-compile "$COMPILE_INPUT" "$TRIE" \
     | diff -q - "$COMPILE_OUTPUT"
 
-libtool --mode=execute valgrind --error-exitcode=1 -q ./list-query $TRIE <"$QUERY_INPUT" \
+libtool --mode=execute $VALGRIND ./list-query $TRIE <"$QUERY_INPUT" \
     | diff -q - "$QUERY_OUTPUT"
 
 rm -f $TRIE $COMPILE_INPUT $COMPILE_OUTPUT $QUERY_INPUT $QUERY_OUTPUT
--- tests/integration/test03.sh
+++ tests/integration/test03.sh
@@ -3,6 +3,13 @@
 set -e
 set -o pipefail
 
+if [ "x${USE_VALGRIND:-yes}" == "xno" ]; then
+    VALGRIND=""
+else
+    VALGRIND="valgrind --error-exitcode=1 -q"
+fi
+
+
 TRIE=$(mktemp)
 COMPILE_INPUT=$(mktemp)
 COMPILE_OUTPUT=$(mktemp)
@@ -33,10 +40,10 @@ EOF
 
 make
 
-libtool --mode=execute valgrind --error-exitcode=1 -q ./list-compile -e "$COMPILE_INPUT" "$TRIE" \
+libtool --mode=execute $VALGRIND ./list-compile -e "$COMPILE_INPUT" "$TRIE" \
     | diff -q - "$COMPILE_OUTPUT"
 
-libtool --mode=execute valgrind --error-exitcode=1 -q ./list-query $TRIE <"$QUERY_INPUT" \
+libtool --mode=execute $VALGRIND ./list-query $TRIE <"$QUERY_INPUT" \
     | diff -q - "$QUERY_OUTPUT"
 
 rm -f $TRIE $COMPILE_INPUT $COMPILE_OUTPUT $QUERY_INPUT $QUERY_OUTPUT
