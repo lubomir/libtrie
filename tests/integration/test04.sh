bab5a644ff8b734a72d816218edb7cc08eb61349
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2014-04-04 13:33:19 +0200

    Increase number of tests in test 4

--- tests/integration/test04.sh
+++ tests/integration/test04.sh
@@ -2,12 +2,14 @@
 
 . $(dirname $0)/helper.sh
 
-strings </dev/urandom | head -n1000 | compile_input
+COUNT=10000
+
+strings </dev/urandom | head -n $COUNT | compile_input
 
 num_keys=$(wc -l <$COMPILE_INPUT)
 echo "Inserted $num_keys items" | compile_output
 
 query_input <$COMPILE_INPUT
-yes "Found" | head -n 1000 | query_output
+yes "Found" | head -n $COUNT | query_output
 
 runtest "-e"
