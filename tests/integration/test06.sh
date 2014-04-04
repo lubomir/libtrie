f862c9f624655caa18594bddc69c97086dad88da
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2014-04-04 13:40:54 +0200

    Add test with very long data

---
+++ tests/integration/test06.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+. $(dirname $0)/helper.sh
+
+COUNT=10000
+
+yes "very-very-very-long-key:some data" | head -n $COUNT | compile_input
+echo "Inserted $COUNT items" | compile_output
+
+echo "very-very-very-long-key" | query_input
+yes "some data" | head -n $COUNT | query_output
+
+runtest ""
