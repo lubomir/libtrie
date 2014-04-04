8c9f3513dee806a2f8adeab15f5686e2f927c43a
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2014-04-04 14:12:39 +0200

    Lookup random keys in different order to inserting

--- tests/integration/random-keys.sh
+++ tests/integration/random-keys.sh
@@ -9,7 +9,11 @@ strings </dev/urandom | head -n $COUNT | compile_input
 num_keys=$(wc -l <$COMPILE_INPUT)
 echo "Inserted $num_keys items" | compile_output
 
-query_input <$COMPILE_INPUT
+SHUF=shuf
+if ! which shuf >/dev/null; then
+    SHUF=cat
+fi
+$SHUF $COMPILE_INPUT | query_input
 yes "Found" | head -n $COUNT | query_output
 
 runtest "-e"
