69a4eb0891bd045b001b3f26fb0c006bb8506c9f
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2014-04-05 13:31:43 +0200

    Add test for disabled compression

---
+++ tests/integration/custom-delimiter-no-compress.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+. $(dirname $0)/helper.sh
+
+compile_input <<EOF
+foo|bar
+baz|quux
+ahoj|baf
+foo|foo
+EOF
+
+compile_output <<EOF
+Inserted 4 items
+EOF
+
+query_input << EOF
+foo
+baz
+non
+EOF
+
+query_output <<EOF
+bar
+foo
+quux
+Not found
+EOF
+
+runtest "-d'|' -u"
---
+++ tests/integration/very-many-keys-no-compress.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+. $(dirname $0)/helper.sh
+
+COUNT=10000
+
+for n in $(seq 1 $COUNT); do
+    echo "my-key-$n:my-data-$n"
+done | $SHUF | compile_input
+echo "Inserted $COUNT items" | compile_output
+
+cut -d: -f1 $COMPILE_INPUT | query_input
+cut -d: -f2 $COMPILE_INPUT | query_output
+
+runtest "-u"
